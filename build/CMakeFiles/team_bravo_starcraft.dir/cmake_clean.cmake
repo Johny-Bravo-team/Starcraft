dfb6a2b3c386640ddda533f095fab9c7eeab8d34
Iordan Tonchev <issashu@icloud.com>
2021-03-25 12:20:00 +0200

    commit cmake tests
    Co-authored-by: Desislava Velkova <Desislava-Velkova@users.noreply.github.com>

---
+++ build/CMakeFiles/team_bravo_starcraft.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/team_bravo_starcraft.dir/src/BattleField.c.o"
+  "CMakeFiles/team_bravo_starcraft.dir/src/Vector.c.o"
+  "CMakeFiles/team_bravo_starcraft.dir/src/main.c.o"
+  "team_bravo_starcraft"
+  "team_bravo_starcraft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/team_bravo_starcraft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
