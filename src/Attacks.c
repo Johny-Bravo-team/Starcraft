9f92a7b35b349e3528b22b40ab4113ce0fba2b8f
Iordan Tonchev <issashu@icloud.com>
2021-03-28 09:28:46 +0300

    Function rearranging

--- src/Attacks.c
+++ src/Attacks.c
@@ -8,6 +8,29 @@
 #include "Vector.h"
 #include "Attacks.h"
 
+void Attack(Vector *Attacker, Vector *Defender, int index, int turn){
+    Ship *Attacking_Ship = vectorGet(Attacker, index);
+    Ship *Defending_Ship = vectorBack(Defender);
+    int Damage = CalculateDamage(Attacker, Defender, index, turn);
+    
+    if (!strcmp((*Attacking_Ship).name, "Carrier")){
+        CarrierAttack(Attacking_Ship, Defending_Ship, index, Defender, Damage);
+    }
+    else if (!strcmp((*Attacking_Ship).name, "Phoenix")){
+        TerranTakeDamage(Defending_Ship, Damage);
+    }
+    else if (!strcmp((*Attacking_Ship).name, "Viking") || !strcmp((*Attacking_Ship).name, "BattleCruser")){
+        ProtossTakeDamage(Defending_Ship, Damage);
+    }
+    
+    if (Defending_Ship->health <= 0){
+        free(Defending_Ship);
+        vectorDelete(Defender, vectorGetSize(Defender) - 1);
+        printf("%s with ID: %d killed enemy airship with ID: %d\n", Attacking_Ship->name, index, vectorGetSize(Defender));
+        return;
+    }
+}
+
 void CarrierAttack(Ship *Attacking_Ship, Ship *Defending_Ship, int index, Vector *Defender, int Damage){
     if ((*Attacking_Ship).health == CARRIER_HEALTH){
         for (int i = 1; i <= MAX_INTERCEPTORS; i++){
--- src/GameTurn.c
+++ src/GameTurn.c
@@ -21,25 +21,3 @@ void TakeTurn(Vector *Attacker, Vector *Defender, int TurnCounter){
     }
 }
 
-void Attack(Vector *Attacker, Vector *Defender, int index, int turn){
-    Ship *Attacking_Ship = vectorGet(Attacker, index);
-    Ship *Defending_Ship = vectorBack(Defender);
-    int Damage = CalculateDamage(Attacker, Defender, index, turn);
-    
-    if (!strcmp((*Attacking_Ship).name, "Carrier")){
-        CarrierAttack(Attacking_Ship, Defending_Ship, index, Defender, Damage);
-    }
-    else if (!strcmp((*Attacking_Ship).name, "Phoenix")){
-        TerranTakeDamage(Defending_Ship, Damage);
-    }
-    else if (!strcmp((*Attacking_Ship).name, "Viking") || !strcmp((*Attacking_Ship).name, "BattleCruser")){
-        ProtossTakeDamage(Defending_Ship, Damage);
-    }
-    
-    if (Defending_Ship->health <= 0){
-        free(Defending_Ship);
-        vectorDelete(Defender, vectorGetSize(Defender) - 1);
-        printf("%s with ID: %d killed enemy airship with ID: %d\n", Attacking_Ship->name, index, vectorGetSize(Defender));
-        return;
-    }
-}
